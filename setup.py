47218b7431965927be8001c9d320820f1678bbef
Alex <liu_zhao_feng_alex@163.com>
2022-09-24 17:49:17 +0800

    測試自動發佈

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nonebot_plugin_tuling",
-    version="0.0.1",
+    version="0.0.12",
     author="alex",
     author_email="liu_zhao_feng_alex@163.com",
     description="nonebot_plugin_tuling",
